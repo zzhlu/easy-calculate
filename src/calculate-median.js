d07d72a3161263d13c110e7d4a38c23d26faba3a
zhaolu <1632089314@qq.com>
2016-06-10 14:53:06 +0800

    finished

--- src/calculate-median.js
+++ src/calculate-median.js
@@ -1,13 +1,17 @@
 function calculate_median(arr) {
     var evenNumbers = [];
+    var sum = 0;
 
     arr.forEach(function (number) {
-        if(number % 2 === 0) {
+        if (number % 2 === 0) {
             evenNumbers.push(number);
         }
     });
+    evenNumbers.forEach(function (evenNumber) {
+        sum += evenNumber;
+    });
 
-    return (evenNumbers[0] + evenNumbers[evenNumbers.length-1]) / 2;
+    return sum / evenNumbers.length;
 }
 
 module.exports = calculate_median;
